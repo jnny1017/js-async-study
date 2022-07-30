9e7e92780ad5a2212bb587345162450dbe95328d
jnny1017 <jnny1017@gmail.com>
2022-07-30 11:11:48 +0900

    fs.readFile을 promise로 처리할 수 있게 바꾼 readFile을 구현

--- src/readFile.js
+++ src/readFile.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const fs_promise = fs.promises;
+
 export const readFile = (filePath) => {
-  // return Promise
-  // TODO
-  throw Error("fs.readFile을 이용해 Promise화 시킨 readFile을 구현하여 보세요");
-  // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(data);
+        });
+    });
 };
