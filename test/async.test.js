c78eb05f73da5e9f05ea3db702a54ab6e4f9d4d2
jnny1017 <jnny1017@gmail.com>
2022-07-30 11:12:10 +0900

    readFile을 이용해 async/await 패턴으로 테스트 코드 작성

--- test/async.test.js
+++ test/async.test.js
@@ -1,18 +1,47 @@
 const fs = require("fs");
+const { readFile } = require("../src/readFile");
 
 describe("async/await - readFile함수를 async/await 만 사용하여 테스트 통과하기", () => {
-  test("assets/first.json을 불러오기", (done) => {
-    const result = "테스트 코드 작성하기";
-    expect(result).toBe("Second 방가방가");
-  });
+    test("assets/first.json을 불러오기", async function () {
+        const data = await readFile("assets/first.json");
 
-  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", () => {
-    const result = "테스트 코드 작성하기";
-    expect(result).toBe("Second 방가방가");
-  });
+        const first = JSON.parse(data);
 
-  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", () => {
-    const result = "테스트 코드 작성하기";
-    expect(result).toBe("Third 방가방가");
+        expect(first.hi).toContain("방가방가");
+    });
+
+    test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기",  async function () {
+        // first
+        const data = await readFile("assets/first.json");
+        const firstData = JSON.parse(data);
+        const secondKey = firstData.second_key; // first.json에서 불러온 second_key (1)
+
+        // second
+        const data2 = await readFile("assets/second.json");
+        const secondData = JSON.parse(data2);
+
+        const result = secondData.filter((item) => item.key === secondKey);
+
+        expect(result[0].hi).toBe("Second 방가방가");
+    });
+
+  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", async function () {
+    // first
+    const data = await readFile("assets/first.json");
+    const firstData = JSON.parse(data);
+    const secondKey = firstData.second_key; // first.json에서 불러온 second_key (1)
+
+    // second
+    const data2 = await readFile("assets/second.json");
+    const secondData = JSON.parse(data2);
+    const secondResult = secondData.filter((item) => item.key === secondKey);
+
+    // third
+    const data3 = await readFile("assets/third.json");
+    const thirdData = JSON.parse(data3);
+    const thirdKey = secondResult[0].third_key // second.json에서 불러온 third_key
+    const thirdResult = thirdData.filter((item) => item.key === thirdKey);
+
+    expect(thirdResult[0].hi).toBe("Third 방가방가");
   });
 });
